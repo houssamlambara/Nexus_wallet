78b2712a5399f838c41c8d34db81c5d2e2949879
S-al1 <salmaaitabbou972@gmail.comi>
2025-02-11 19:21:03 +0100

    rebase public and fixing App and index.php

--- app/core/App.php
+++ app/core/App.php
@@ -7,7 +7,7 @@
 
         public function __construct(){
             $url = $this->parseUrl();
-            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+            if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
                 $this->controller = ucwords($url[0]);
                 unset($url[0]);
             }
