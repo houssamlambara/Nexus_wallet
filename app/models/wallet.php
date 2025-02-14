0552b166b91471c2af8e7bafb2f8dbc52f5f4fe8
mohamedelasri202 <el7994995@gmail.com>
2025-02-14 17:31:40 +0100

    upload the code

--- app/models/wallet.php
+++ app/models/wallet.php
@@ -47,15 +47,14 @@ class wallet extends Controller {
 
         public function userWallet($idUser) {
             $conn = DatabaseConnection::getInstance()->getConnection();
-            $crypto = $conn->prepare("
-            SELECT 
-                c.name AS crypto_name, 
-                c.symbol, 
-                w.balance
-            FROM wallets w
-            JOIN cryptos c ON w.crypto_id = c.id
-            WHERE w.user_id = :user_id;
-        ");
+            $crypto = $conn->prepare($query = "SELECT 
+            c.name AS crypto_name, 
+            c.symbol, 
+            w.balance
+          FROM wallets w
+          JOIN cryptos c ON w.crypto_id = c.id::VARCHAR
+          WHERE w.user_id = :user_id"
+        );
             $crypto->bindParam(":user_id", $idUser);
             $crypto->execute();
             return $crypto->fetchAll(PDO::FETCH_ASSOC);
