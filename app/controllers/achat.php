cd23d012761c20f496a895723ab9fa8177919f94
Wassim <wassimwassim.ka1@gmail.com>
2025-02-14 14:30:36 +0100

    fix achat page

--- app/controllers/achat.php
+++ app/controllers/achat.php
@@ -3,10 +3,7 @@
     class achat extends Controller{
 
         public function index(){
-<<<<<<< HEAD
 
-=======
->>>>>>> 9f6a7c97a0315bdda88b9e56937edd9b7cf21cf6
             $this->view('pages/achat',[]);
         }
     }
